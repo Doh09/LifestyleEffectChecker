10d567f66bfbee222c724cefb66027b99ba14d4e
Doh09 <alexandreasen09@gmail.com>
2017-03-20 13:23:52 +0100

    Initial commit

---
+++ LifestyleEffectChecker/LifestyleEffectChecker/Views/NewItemPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using LifestyleEffectChecker.Models;
+
+using Xamarin.Forms;
+
+namespace LifestyleEffectChecker.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+
+            Item = new Item
+            {
+                Text = "Item name",
+                Description = "This is a nice description"
+            };
+
+            BindingContext = this;
+        }
+
+        async void Save_Clicked(object sender, EventArgs e)
+        {
+            MessagingCenter.Send(this, "AddItem", Item);
+            await Navigation.PopToRootAsync();
+        }
+    }
+}
