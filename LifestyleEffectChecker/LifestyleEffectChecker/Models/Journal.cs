42d2e2163b7fcfef15464efb953912196fc35c41
MartinDataStuff <martin1-g@hotmail.com>
2017-03-23 09:55:11 +0100

    Added Journal and it's repository

---
+++ LifestyleEffectChecker/LifestyleEffectChecker/Models/Journal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifestyleEffectChecker.Models
+{
+    class Journal : AbstractBaseObject
+    {
+        public string Name { get; set; }
+
+        public List<Action> ActionParts { get; set; } = new List<Action>();
+    }
+}
---
+++ LifestyleEffectChecker/LifestyleEffectChecker/Repository/JournalRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LifestyleEffectChecker.Connection;
+using LifestyleEffectChecker.Models;
+using SQLite.Net;
+using Xamarin.Forms;
+
+namespace LifestyleEffectChecker.Repository
+{
+    class JournalRepository : IRepository<Journal>
+    {
+        private readonly SQLiteConnection _connection;
+
+        public JournalRepository(SQLiteConnection connection)
+        {
+            _connection = DependencyService.Get<IDBConnection>().GetConnection();
+            _connection.CreateTable<Journal>();
+        }
+
+        public Journal Create(Journal obj)
+        {
+            _connection.Insert(obj);
+            return obj;
+        }
+
+        public Journal Read(int id)
+        {
+            return _connection.Table<Journal>().FirstOrDefault(action => action.ID == id);
+        }
+
+        public IEnumerable<Journal> ReadAll()
+        {
+            return (from t in _connection.Table<Journal>() select t).ToList();
+        }
+
+        public Journal Update(Journal obj)
+        {
+            _connection.Update(obj);
+            return obj;
+        }
+
+        public bool Delete(int id)
+        {
+            _connection.Delete<Journal>(id);
+            return Read(id) != null;
+        }
+    }
+}
