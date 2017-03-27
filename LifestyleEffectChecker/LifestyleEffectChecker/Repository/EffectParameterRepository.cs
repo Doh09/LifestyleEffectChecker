77a6e08b812d4e3d140a54f22aafb2137821b0a4
MartinDataStuff <martin1-g@hotmail.com>
2017-03-27 15:25:46 +0200

    Added repository for the effect classes

---
+++ LifestyleEffectChecker/LifestyleEffectChecker/Repository/EffectParameterRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LifestyleEffectChecker.Connection;
+using LifestyleEffectChecker.Models;
+using SQLite.Net;
+using Xamarin.Forms;
+
+namespace LifestyleEffectChecker.Repository
+{
+    class EffectParameterRepository : IRepository<EffectParameter>
+    {
+        private readonly SQLiteConnection _connection;
+
+        public EffectParameterRepository()
+        {
+            _connection = DependencyService.Get<IDBConnection>().GetConnection();
+            _connection.CreateTable<EffectParameter>();
+        }
+
+        public async Task<EffectParameter> Create(EffectParameter obj)
+        {
+            _connection.Insert(obj);
+            return await Task.FromResult(obj);
+        }
+
+        public async Task<EffectParameter> Read(int id)
+        {
+            return await Task.FromResult(_connection.Table<EffectParameter>().FirstOrDefault(action => action.ID == id));
+        }
+
+        public async Task<IEnumerable<EffectParameter>> ReadAll()
+        {
+            return await Task.FromResult((from t in _connection.Table<EffectParameter>() select t).ToList());
+        }
+
+        public async Task<EffectParameter> Update(EffectParameter obj)
+        {
+            _connection.Update(obj);
+            return await Task.FromResult(obj);
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            _connection.Delete<EffectParameter>(id);
+            return await Task.FromResult(Read(id) != null);
+        }
+    }
+}
---
+++ LifestyleEffectChecker/LifestyleEffectChecker/Repository/EffectRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LifestyleEffectChecker.Connection;
+using LifestyleEffectChecker.Models;
+using SQLite.Net;
+using Xamarin.Forms;
+using Effect = LifestyleEffectChecker.Models.Effect;
+
+namespace LifestyleEffectChecker.Repository
+{
+    class EffectRepository : IRepository<Effect>
+    {
+        private readonly SQLiteConnection _connection;
+
+        public EffectRepository()
+        {
+            _connection = DependencyService.Get<IDBConnection>().GetConnection();
+            _connection.CreateTable<Effect>();
+        }
+
+        public async Task<Effect> Create(Effect obj)
+        {
+            _connection.Insert(obj);
+            return await Task.FromResult(obj);
+        }
+
+        public async Task<Effect> Read(int id)
+        {
+            return await Task.FromResult(_connection.Table<Effect>().FirstOrDefault(action => action.ID == id));
+        }
+
+        public async Task<IEnumerable<Effect>> ReadAll()
+        {
+            return await Task.FromResult((from t in _connection.Table<Effect>() select t).ToList());
+        }
+
+        public async Task<Effect> Update(Effect obj)
+        {
+            _connection.Update(obj);
+            return await Task.FromResult(obj);
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            _connection.Delete<EffectParameter>(id);
+            return await Task.FromResult(Read(id) != null);
+        }
+
+    }
+}
