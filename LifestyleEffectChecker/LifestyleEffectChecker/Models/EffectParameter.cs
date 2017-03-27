879ddf871b1439fce366b8e9ff0dd7246b4eb0c0
Doh09 <alexandreasen09@gmail.com>
2017-03-27 15:29:36 +0200

    Moved ValueContainer into EffectParameter as it was excessive class separation
    Also added a GetValue method to the EffectParameter, which is based on its MeasuringMethod.

--- LifestyleEffectChecker/LifestyleEffectChecker/Models/EffectParameter.cs
+++ LifestyleEffectChecker/LifestyleEffectChecker/Models/EffectParameter.cs
@@ -3,17 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Plugin.Geolocator.Abstractions;
 
 namespace LifestyleEffectChecker.Models
 {
     public class EffectParameter : AbstractBaseObject
     {
-        
+
         public string Name { get; set; }
+        public MeasuringMethod MeasuringMethod { get; set; } = MeasuringMethod.Slider;
 
-        public MeasuringMethod Method { get; set; } = MeasuringMethod.Slider;
+        private string Text { get; set; }
+        private string ImgPath { get; set; }
+        private double DecimalValue { get; set; }
+        private double SliderValue { get; set; }
+        private int NumberValue { get; set; }
+        private Position GeoPosition { get; set; }
 
-        public object Value { get; set; }
+        public string GetValue()
+        {
+            switch (MeasuringMethod)
+            {
+                case MeasuringMethod.Text:
+                    return Text;
+                case MeasuringMethod.Decimal:
+                    return DecimalValue+"";
+                case MeasuringMethod.Number:
+                    return NumberValue + "";
+                case MeasuringMethod.Picture:
+                    return ImgPath;
+                case MeasuringMethod.Slider:
+                    return SliderValue+"";
+                    case MeasuringMethod.GPSLocation:
+                    return "GeoLocation - Latitude:" + GeoPosition.Latitude + " " + "Longitude:" +
+                           GeoPosition.Longitude;
+                default:
+                    return "No measuring method defined";
+            }
+        }
     }
 
     public enum MeasuringMethod
